6f46009d50ffac344e2b989a3282f4a3025bb84d
seonghwanjo <joseonghwan3021@gmail.com>
2024-12-12 15:31:52 +0900

    #7.2 Date Flow

--- Home.py
+++ Home.py
@@ -1,23 +1,20 @@
+from datetime import datetime
+
 import streamlit as st
 
-st.set_page_config(
-    page_title="FullstackGPT Home",
-    page_icon="🤖",
+today = datetime.today().strftime('%H:%M:%S')
+
+st.title(today)
+
+model = st.selectbox(
+    "Choose your model!",
+    ("GPT-3", "GPT-4",),
 )
 
-st.markdown(
-    """
-    # Hello!
-                
-    ### Welcome to my FullstackGPT Portfolio!
-                
-    ##### Here are the apps I made:
+st.write(model)
 
-    - [ ] [DocumentGPT](/DocumentGPT)
-    - [ ] [PrivateGPT](/PrivateGPT)
-    - [ ] [QuizGPT](/QuizGPT)
-    - [ ] [SiteGPT](/SiteGPT)
-    - [ ] [MeetingGPT](/MeetingGPT)
-    - [ ] [InvestorGPT](/InvestorGPT)
-    """
-)
+name = st.text_input("Enter your name")
+st.write(f"Hello {name or 'Anonymous'}!")
+
+value = st.slider('temperature', min_value=0.1, max_value=1.0)
+st.write(value)
