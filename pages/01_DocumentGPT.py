02a70d44ec1dff8992eb26572e56abdbfe1bb0df
seonghwanjo <joseonghwan3021@gmail.com>
2024-12-16 12:19:52 +0900

    #7.7 Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,6 +1,3 @@
-import stat
-import time
-
 import streamlit as st
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
@@ -8,7 +5,17 @@ from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📄",
+    layout="wide",
+)
 
+if "messages" not in st.session_state:
+    st.session_state['messages'] = []
+
+
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f'./.cache/files/{file.name}'
@@ -30,6 +37,18 @@ def embed_file(file):
     return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state['messages'].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state['messages']:
+        send_message(message['message'], message['role'], save=False)
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -37,14 +56,23 @@ st.markdown(
     Welcome!
 
     Use this chatbot to ask questions to an AI about a document.
+
+    Upload a .txt, .pdf, or .docx file at the sidebar to get started.
     """
 )
 
-file = st.file_uploader('Upload a .txt, .pdf, or .docx file', type=[
-                        'txt', 'pdf', 'docx'])
+with st.sidebar:
+    file = st.file_uploader('Upload a .txt, .pdf, or .docx file', type=[
+        'txt', 'pdf', 'docx'])
 
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke('winston')
-    st.write(s)
+    send_message("I'm ready to answer your questions!", "AI", save=False)
+    paint_history()
+    message = st.chat_input("Ask me a question")
+    if message:
+        send_message(message, "Human")
+        send_message('I am thinking...', "AI")
+else:
+    st.session_state['messages'] = []
